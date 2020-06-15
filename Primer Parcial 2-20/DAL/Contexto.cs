760a1a690a1ba6ad83f7a1389f21bfe172701860
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-06-15 18:11:57 -0400

    Agrege la case Contexto y esta herendado de DbContext

---
+++ Primer Parcial 2-20/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Primer_Parcial_2_20.DAL
+{
+   public class Contexto : DbContext 
+    {
+        public DbSet<Entidades.Usuarios> Usuarios { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuider)
+        {
+            optionsBuider.UseSqlite(@"Data source = Data\Usuarios.db");
+        }
+    }
+}
